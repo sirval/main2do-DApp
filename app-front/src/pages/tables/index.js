5edcba741a19e39309023f14a3518519fc18399b
Ikenna Valentine <ohukaiv@gmail.com>
2023-05-06 10:15:36 +0100

    updated code for deployment

--- app-front/src/pages/tables/index.js
+++ app-front/src/pages/tables/index.js
@@ -1,17 +1,17 @@
 // ** MUI Imports
-import Grid from '@mui/material/Grid'
-import Link from '@mui/material/Link'
-import Card from '@mui/material/Card'
-import Typography from '@mui/material/Typography'
-import CardHeader from '@mui/material/CardHeader'
+import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
+import Card from '@mui/material/Card';
+import Typography from '@mui/material/Typography';
+import CardHeader from '@mui/material/CardHeader';
 
 // ** Demo Components Imports
-import TableBasic from 'src/views/tables/TableBasic'
-import TableDense from 'src/views/tables/TableDense'
-import TableSpanning from 'src/views/tables/TableSpanning'
-import TableCustomized from 'src/views/tables/TableCustomized'
-import TableCollapsible from 'src/views/tables/TableCollapsible'
-import TableStickyHeader from 'src/views/tables/TableStickyHeader'
+import TableBasic from 'src/views/tables/TableBasic';
+import TableDense from 'src/views/tables/TableDense';
+import TableSpanning from 'src/views/tables/TableSpanning';
+import TableCustomized from 'src/views/tables/TableCustomized';
+import TableCollapsible from 'src/views/tables/TableCollapsible';
+import TableStickyHeader from 'src/views/tables/TableCollapsible';
 
 const MUITable = () => {
   return (
@@ -22,7 +22,9 @@ const MUITable = () => {
             MUI Tables
           </Link>
         </Typography>
-        <Typography variant='body2'>Tables display sets of data. They can be fully customized</Typography>
+        <Typography variant='body2'>
+          Tables display sets of data. They can be fully customized
+        </Typography>
       </Grid>
       <Grid item xs={12}>
         <Card>
@@ -61,7 +63,7 @@ const MUITable = () => {
         </Card>
       </Grid>
     </Grid>
-  )
-}
+  );
+};
 
-export default MUITable
+export default MUITable;
--- app-front/src/views/cards/CardTwitter.js
+++ app-front/src/views/cards/CardTwitter.js
@@ -1,24 +1,24 @@
-import {useState} from 'react'
+import { useState } from 'react';
 // ** MUI Imports
-import Grid from '@mui/material/Grid'
-import Box from '@mui/material/Box'
-import Card from '@mui/material/Card'
-import Avatar from '@mui/material/Avatar'
-import Typography from '@mui/material/Typography'
-import CardContent from '@mui/material/CardContent'
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import CardContent from '@mui/material/CardContent';
 
-import MenuItem from '@mui/material/MenuItem'
-import TextField from '@mui/material/TextField'
-import InputLabel from '@mui/material/InputLabel'
-import FormControl from '@mui/material/FormControl'
-import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem';
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 // ** Icons Imports
-import Heart from 'mdi-material-ui/Heart'
-import Twitter from 'mdi-material-ui/Twitter'
-import ShareVariant from 'mdi-material-ui/ShareVariant'
-import Button from '@mui/material/Button'
-import { Timer, TimerSandEmpty, TimerSandPaused, TimerOff } from 'mdi-material-ui'
+import Heart from 'mdi-material-ui/Heart';
+import Twitter from 'mdi-material-ui/Twitter';
+import ShareVariant from 'mdi-material-ui/ShareVariant';
+import Button from '@mui/material/Button';
+import { Timer, TimerSandEmpty, TimerSandPaused, TimerOff } from 'mdi-material-ui';
 
 import Modal from '@mui/material/Modal';
 
@@ -33,79 +33,74 @@ const style = {
   boxShadow: 24,
   p: 4,
 };
-const CardTwitter = props => {
+const CardTwitter = (props) => {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   // console.log(props)
   const pageName = () => {
     if (props.pageName == 'cancelled') {
-      return '#ff7663'
+      return '#ff7663';
     }
     if (props.pageName == 'ongoing') {
-      return 'info.main'
+      return 'info.main';
     }
     if (props.pageName == 'completed') {
-      return 'green'
+      return 'green';
     }
     if (props.pageName == 'pending') {
-      return 'orange'
+      return 'orange';
     }
 
     if (props.pageName == 'home') {
-      return 'yellow'
+      return 'yellow';
     }
-  }
+  };
 
   const getTodoPriority = (priority) => {
-    let todo_priority = parseInt(priority.toString())
-    if(todo_priority === 1)return 'Very High'
-    if(todo_priority === 2)return 'High'
-    if(todo_priority === 3)return 'Medium'
-    if(todo_priority === 3)return 'Low'
-      // uint256 status // 1=ongoing, 2=completed, 3=pending, 4=cancelled
-    
-  }
+    let todo_priority = parseInt(priority.toString());
+    if (todo_priority === 1) return 'Very High';
+    if (todo_priority === 2) return 'High';
+    if (todo_priority === 3) return 'Medium';
+    if (todo_priority === 3) return 'Low';
+    // uint256 status // 1=ongoing, 2=completed, 3=pending, 4=cancelled
+  };
 
   const getTodoStatus = (status) => {
-    let todo_status = parseInt(status.toString())
-    if(todo_status === 1)return 'Ongoing'
-    if(todo_status === 2)return 'Completed'
-    if(todo_status === 3)return 'Pending'
-    if(todo_status === 4)return 'Cancelled'
-      // uint256 status // 1=ongoing, 2=completed, 3=pending, 4=cancelled
-    
-  }
+    let todo_status = parseInt(status.toString());
+    if (todo_status === 1) return 'Ongoing';
+    if (todo_status === 2) return 'Completed';
+    if (todo_status === 3) return 'Pending';
+    if (todo_status === 4) return 'Cancelled';
+    // uint256 status // 1=ongoing, 2=completed, 3=pending, 4=cancelled
+  };
 
+  const [formData, setFormData] = useState({});
+  const [errors, setErrors] = useState({});
 
-  const [formData, setFormData] = useState({})
-  const [errors, setErrors] = useState({})
+  const handleFormSubmit = (evt) => {
+    evt.preventDefault();
+    const errors = {};
 
-  const handleFormSubmit = evt => {
-    evt.preventDefault()
-    const errors = {}
-
-  
     if (!formData.status) {
-      errors.status = 'Todo status is required'
+      errors.status = 'Todo status is required';
     }
-
 
     if (Object.keys(errors).length > 0) {
-      setErrors(errors)
+      setErrors(errors);
 
-      return
+      return;
     }
-    console.log(formData)
-  }
+    console.log(formData);
+  };
 
-  const handleChange = evt => {
-    setFormData({ ...formData, [evt.target.name]: evt.target.value })
-  }
+  const handleChange = (evt) => {
+    setFormData({ ...formData, [evt.target.name]: evt.target.value });
+  };
 
   return (
     <Card sx={{ border: 0, boxShadow: 0, color: 'common.white', backgroundColor: pageName() }}>
-      <CardContent sx={{ padding: theme => `${theme.spacing(3.25, 5, 4.5)} !important` }}>
+      <CardContent sx={{ padding: (theme) => `${theme.spacing(3.25, 5, 4.5)} !important` }}>
         <Typography
           variant='h6'
           sx={{ display: 'flex', marginBottom: 3, alignItems: 'center', color: 'common.white' }}
@@ -113,66 +108,87 @@ const CardTwitter = props => {
           {props.title}
         </Typography>
         <Typography variant='body2' sx={{ marginBottom: 4, color: 'common.white' }}>
-           <span sx={{ marginRight: 2.5 }} >Description: </span>
-            {props.description}
+          <span sx={{ marginRight: 2.5 }}>Description: </span>
+          {props.description}
         </Typography>
         <Typography variant='body2' sx={{ marginBottom: 4, color: 'common.white' }}>
-            <span sx={{ marginRight: 2.5 }} style={{backgroundColor: 'red', borderRadius: '5px', padding: '2px'}}><b>Priority </b></span>&nbsp; {getTodoPriority(props?.priority)}
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <span sx={{ marginRight: 2.5 }} style={{backgroundColor: 'red', borderRadius: '5px', padding: '2px'}}><b>Status </b></span>&nbsp; {getTodoStatus(props?.status)}
+          <span
+            sx={{ marginRight: 2.5 }}
+            style={{ backgroundColor: 'red', borderRadius: '5px', padding: '2px' }}
+          >
+            <b>Priority </b>
+          </span>
+          &nbsp; {getTodoPriority(props?.priority ?? 1)}
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <span
+            sx={{ marginRight: 2.5 }}
+            style={{ backgroundColor: 'red', borderRadius: '5px', padding: '2px' }}
+          >
+            <b>Status </b>
+          </span>
+          &nbsp; {getTodoStatus(props?.status ?? 1)}
         </Typography>
         <Typography variant='body2' sx={{ marginBottom: 4, color: 'common.white' }}>
-            <span sx={{ marginRight: 2.5 }} style={{backgroundColor: 'red', borderRadius: '5px', padding: '2px'}}><b>Time</b></span>&nbsp;{props.todoTime} &nbsp;&nbsp;
+          <span
+            sx={{ marginRight: 2.5 }}
+            style={{ backgroundColor: 'red', borderRadius: '5px', padding: '2px' }}
+          >
+            <b>Time</b>
+          </span>
+          &nbsp;{props.todoTime} &nbsp;&nbsp;
         </Typography>
 
+        <Typography variant='body2' sx={{ marginBottom: 3, color: 'common.white' }}></Typography>
+        <Box
+          sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Button onClick={handleOpen}>Update Status</Button>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby='modal-modal-title'
+            aria-describedby='modal-modal-description'
+          >
+            <Box sx={style}>
+              <form onSubmit={handleFormSubmit}>
+                <>
+                  <Grid item xs={12} sm={12} sx={{ marginBottom: 5 }}>
+                    <FormControl fullWidth>
+                      <InputLabel>Choose Status</InputLabel>
+                      <Select
+                        label='Choose Status'
+                        defaultValue='1'
+                        name='status'
+                        onChange={handleChange}
+                        value={formData.status || ''}
+                        error={!!errors.status}
+                        helperText={errors.status}
+                      >
+                        <MenuItem value='1'>Ongoing</MenuItem>
+                        <MenuItem value='2'>Completed</MenuItem>
+                        <MenuItem value='3'>Pending</MenuItem>
+                        <MenuItem value='4'>Cancelled</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid>
 
-        <Typography variant='body2' sx={{ marginBottom: 3, color: 'common.white' }}>
-            
-        </Typography>
-        <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'space-between' }}>
-        <Button onClick={handleOpen}>Update Status</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-       <Box sx={style}>
-      <form onSubmit={handleFormSubmit}>
-        <>
-          <Grid item xs={12} sm={12} sx={{ marginBottom: 5 }}>
-            <FormControl fullWidth>
-              <InputLabel>Choose Status</InputLabel>
-              <Select
-                label='Choose Status'
-                defaultValue='1'
-                name='status'
-                onChange={handleChange}
-                value={formData.status || ''}
-                error={!!errors.status}
-                helperText={errors.status}
-              >
-                <MenuItem value='1'>Ongoing</MenuItem>
-                <MenuItem value='2'>Completed</MenuItem>
-                <MenuItem value='3'>Pending</MenuItem>
-                <MenuItem value='4'>Cancelled</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-
-
-          <Grid item xs={12}>
-            <Button variant='contained' sx={{ marginRight: 3.5 }}>
-              Update Status
-            </Button>
-            <Button variant='outlined' color='secondary' onClick={handleClose}>
-              Cancel
-            </Button>
-          </Grid>
-        </>
-      </form>
-      </Box>
-      </Modal>
+                  <Grid item xs={12}>
+                    <Button variant='contained' sx={{ marginRight: 3.5 }}>
+                      Update Status
+                    </Button>
+                    <Button variant='outlined' color='secondary' onClick={handleClose}>
+                      Cancel
+                    </Button>
+                  </Grid>
+                </>
+              </form>
+            </Box>
+          </Modal>
           {/* <Button type='submit' variant='contained' style={{ font: '5px', width: 80, backgroundColor: 'yellow'}}>Ongoing</Button>
           <Button type='submit' variant='contained' style={{ font: '5px', width: 100, backgroundColor: 'blue'}}>Completed</Button>
           <Button type='submit' variant='contained' style={{ font: '5px', width: 80, backgroundColor: 'orange'}}>Pending</Button>
@@ -180,7 +196,7 @@ const CardTwitter = props => {
         </Box>
       </CardContent>
     </Card>
-  )
-}
+  );
+};
 
-export default CardTwitter
+export default CardTwitter;
